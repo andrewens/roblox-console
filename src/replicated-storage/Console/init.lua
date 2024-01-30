dc180e1972bcac8658b93827d355c392989d8b17
andrewens <andrewens4@gmail.com>
2024-01-30 10:22:15 -0600

    Console.CharactersPerLine can be get/set to a natural number

--- src/replicated-storage/Console/init.lua
+++ src/replicated-storage/Console/init.lua
@@ -2,23 +2,50 @@
 local function newConsole()
 	-- var
 	local TextBox = Instance.new("TextBox")
+	local charactersPerLine = 16
+
+	local CustomGetMethods = {} -- string customPropertyName --> function(): <any>
+	local CustomSetMethods = {} -- string customPropertyName --> function(value): nil
 
 	-- public
 	local function isConsoleRBXInstance(self, AnyInstance)
 		return TextBox == AnyInstance
 	end
+	local function getCharactersPerLine()
+		return charactersPerLine
+	end
+	local function setCharactersPerLine(anyNaturalNumber)
+		if not (typeof(anyNaturalNumber) == "number") then
+			error(tostring(anyNaturalNumber) .. " is not a number!")
+		end
+		charactersPerLine = math.max(math.floor(anyNaturalNumber), 1)
+	end
+
+	-- public | metamethods
+	local function __index(_, key)
+		if CustomGetMethods[key] then
+			return CustomGetMethods[key]()
+		end
+		return TextBox[key]
+	end
+	local function __newindex(_, key, value)
+		if CustomSetMethods[key] then
+			CustomSetMethods[key](value)
+			return
+		end
+		TextBox[key] = value
+	end
 
 	-- init
+	CustomGetMethods = {
+		CharactersPerLine = getCharactersPerLine,
+	}
+	CustomSetMethods = {
+		CharactersPerLine = setCharactersPerLine,
+	}
 	local mt = {
-		__index = function(_, key)
-			return TextBox[key]
-		end,
-		__newindex = function(_, key, value)
-			TextBox[key] = value
-		end,
-		__eq = function(_, value)
-			return TextBox == value
-		end
+		__index = __index,
+		__newindex = __newindex,
 	}
 	local self = {
 		IsInstance = isConsoleRBXInstance,
--- src/server-script-service/tests/Console.spec.lua
+++ src/server-script-service/tests/Console.spec.lua
@@ -14,7 +14,7 @@ return function()
             error(tostring(v1) .. " ~= " .. tostring(v2))
         end
     end
-    local function shouldThrow(f, msg)
+    local function shouldError(f, msg)
         local s = pcall(f)
         if s then
             error(msg or tostring(f) .. " failed to throw error!")
@@ -26,18 +26,21 @@ return function()
         shouldHaveType(Console.new, "function")
         shouldHaveType(Console.new(), "table")
     end)
-    it("Console.Name can be get & set to strings", function()
+    it("Console.Name can be get/set to strings", function()
         local testName = "MyConsole"
         local MyConsole = Console.new()
 
-        -- console should already have a name property that's a string
+        -- Console.Name should always be a string
         shouldHaveType(MyConsole.Name, "string")
+        shouldError(function()
+            MyConsole.Name = {}
+        end)
 
-        -- console name should be writable
+        -- Console.Name should be writable
         MyConsole.Name = testName
         shouldBeEqual(MyConsole.Name, testName)
     end)
-    it("Console.Parent can be get & set to nil/Instances", function()
+    it("Console.Parent can be get/set to nil/Instances", function()
         local testName = "MyConsole"
         local MyConsole = Console.new()
 
@@ -52,11 +55,11 @@ return function()
         MyConsole.Parent = nil
         shouldBeEqual(MyConsole.Parent, nil)
 
-        -- setting Console.Parent to non-Instance values should error
+        -- Setting Console.Parent to non-Instance values should error
         local badValue = "string"
-        shouldThrow(function()
+        shouldError(function()
             MyConsole.Parent = badValue
-        end, "Setting Console.Parent = " .. tostring(badValue) .. " failed to error")
+        end)
     end)
     it("Console:IsInstance(...) returns true if given the Instance rendering the Console", function()
         local testName = "MyConsole"
@@ -78,4 +81,69 @@ return function()
         shouldBeEqual(MyConsole == ConsoleInstance, false)
         shouldBeEqual(MyConsole == ScreenGui, false)
     end)
+    it("Console.CharactersPerLine can be get/set to a natural number", function()
+        local MyConsole = Console.new()
+
+        -- Console.CharactersPerLine should have a default value of 16
+        shouldHaveType(MyConsole.CharactersPerLine, "number")
+        shouldBeEqual(MyConsole.CharactersPerLine, 16)
+
+        -- Console.CharactersPerLine should be writable
+        local newCharsPerLine = 32
+        MyConsole.CharactersPerLine = newCharsPerLine
+        shouldBeEqual(MyConsole.CharactersPerLine, newCharsPerLine)
+
+        -- Console.CharactersPerLine gets clamped/truncated into a natural number
+        MyConsole.CharactersPerLine = 0
+        shouldBeEqual(MyConsole.CharactersPerLine, 1) -- it gets clamped
+
+        MyConsole.CharactersPerLine = 3.678
+        shouldBeEqual(MyConsole.CharactersPerLine, 3) -- it gets truncated
+
+        -- Console.CharactersPerLine can't be a non-number value
+        shouldError(function()
+            MyConsole.CharactersPerLine = "string"
+        end)
+        shouldError(function()
+            MyConsole.CharactersPerLine = {}
+        end)
+    end)
+
+    --[[
+        History
+
+        ... > Command Line
+
+        (available commands)
+
+        There should be automatic help for the commands...
+
+
+        So as far as this command prompt is concerned, it:
+
+        * Maintains a history buffer of text for the user
+        * Allows inputting commands
+        * Allows for the definition of commands
+
+        Buffer
+        Input
+        Command
+
+        What is a command?
+
+        * A keyword
+        * An operation (fn)
+        * A set of arguments
+        * A set of documentation
+
+        1. Maintain a buffer
+            * Input to it
+                Console:Out(str)
+            * Read from it
+                Console:GetBuffer() --> { str }
+                Console:GetLine()
+            * Draw it on screen somehow
+        2. Get user input
+            Console:In(str)
+    ]]
 end
